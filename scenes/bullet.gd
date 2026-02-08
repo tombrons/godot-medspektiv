5f5c79c7eea8c0dc82ffc6f1458adb8ae285e883
tombrons <toms200lv@gmail.com>
2026-02-08 18:51:19 +0200

    improvements to bow

---
+++ scenes/bullet.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+
+var travelled_distance = 0
+
+func _physics_process(delta: float) -> void:
+	const SPEED = 1000
+	const RANGE = 1600
+	
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
+	
+	travelled_distance += SPEED * delta
+	if travelled_distance >= RANGE:
+		queue_free()
